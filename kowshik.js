b094d3c3b86b9e3164c7f758b518adaf405b6e92
kowshikeys <go2kowshiktouch@gmail.com>
2022-04-25 21:05:14 +0530

    fourth commit

---
+++ kowshik.js
@@ -0,0 +1,12 @@
+const btn=document.querySelector(".buttonX")
+const sidebar=document.querySelector(".sidebar")
+
+function openMenu() {
+    sidebar.style.display="block"
+
+}
+
+function closeMenu() {
+    sidebar.style.display="none"
+
+}
