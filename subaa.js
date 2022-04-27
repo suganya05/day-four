06f69bce441493aa9937e7959fa78f04b1b4d164
Subaa2128 <subaa86@gmail.com>
2022-04-28 00:34:27 +0530

    changes has been  maded in nav and images

---
+++ subaa.js
@@ -0,0 +1,12 @@
+const btn=document.querySelector(".buttonX")
+const bar=document.querySelector(".bar")
+
+function openMenu() {
+    bar.style.display="block"
+
+}
+
+function closeMenu() {
+    bar.style.display="none"
+
+}
